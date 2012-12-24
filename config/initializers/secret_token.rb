a684baacb0677e4b65541fe8c6437ab69e0dcad8
Tom van de Wetering <twetering@gmail.com>
2012-12-24 21:25:21 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComApp::Application.config.secret_token = '48daf63cc83bcee0f132c16ec4b2a9767ea32e5de3040ee34dfd552d69cf7614161371ea142773a63762719abf6d56033bb687dd01156e632e63c5162d7023e5'
